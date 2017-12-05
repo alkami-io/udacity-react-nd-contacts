4f4bbcc59f6e8569e7f6f53fdbb49f6876c18693
alkami-io <robert@alkami.io>
2017-12-05 07:57:57 -0700

    completed sample project

---
+++ src/CreateContact.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from "./ImageInput";
+import serialzeForm from 'form-serialize';
+
+class CreateContact extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const values = serialzeForm(e.target, { hash: true });
+        if (this.props.onCreateContact)
+            this.props.onCreateContact(values)
+    };
+
+    render() {
+        return(
+            <div>
+                <Link className="close-create-contact" to="/">Close</Link>
+                <form onSubmit={this.handleSubmit} className="create-contact-form">
+                    <ImageInput
+                        className='create-contact-avatar-input'
+                        name='avatarURL'
+                        maxHeight={64}
+                    />
+                    <div className='create-contact-details'>
+                        <input type='text' name='name' placeholder='name'/>
+                        <input type='text' name='email' placeholder='email'/>
+                        <button>Add Contact</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+};
+
+export default CreateContact
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,41 +1,57 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
 class ListContacts extends Component {
+    // Defining propTypes for the types of input the application will be receiving.
     static propTypes = {
+        // Ensure that contacts is an array and is required for the application
         contacts: PropTypes.array.isRequired,
+        // Ensure that onDeleteContact is a function and is required for the application
         onDeleteContact: PropTypes.func.isRequired
     };
 
+    // Define state of the component, specifically as it relates to the query string for the search box.
     state = {
       query: ''
     };
 
+    // This function updates the query string by setting the state of the query attribute as it is entered in the input field
     updateQuery = (query) => {
         this.setState({ query: query.trim() })
     };
 
+    // This function clears query or sets the state to an empty string when the show all link/button is clicked
     clearQuery = () => {
         this.setState({ query: '' })
     };
 
+    // Render function as needed by all react components
     render() {
+        // Define specific variables and where they are held.  In this case both of these variables are held in this.prop and this allows you to call contacts instead of this.props.contacts.  makes the code cleaner
         const { contacts, onDeleteContact } = this.props;
+        // This defines the variable of query which is housed in this.state.
         const { query } = this.state;
 
+        // this creates a variable that returns a different variable based on the use of regexp used in the input field.
         let showingContacts;
         if(query) {
+            // if one of the array values matches the regexp that is entered then filter and return the ones that match
             const match = new RegExp(escapeRegExp(query), 'i')
             showingContacts = contacts.filter((contact) => match.test(contact.name) )
         } else {
+            // if no regexp exists return the array as it has been provided.
             showingContacts = contacts
         }
 
+        // before using the showingContacts variable sort them by name so that they always appear in aphabetical order even when they are being sorted.
         showingContacts.sort(sortBy('name'));
 
+        // Return value of what is being sent back to the DOM for rendering
         return (
+            // This is the wrapper for the entire app component.  Only one element can be returned so if there are nested HTML elements make sure there is a parent element that is the primary return element
             <div className="list-contacts">
                 <div className="list-contacts-top">
                     <input
@@ -45,6 +61,10 @@ class ListContacts extends Component {
                         value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
+                    <Link
+                        to='/create'
+                        className="add-contact"
+                    >Add Contact</Link>
                 </div>
 
                 { showingContacts.length !== contacts.length && (
